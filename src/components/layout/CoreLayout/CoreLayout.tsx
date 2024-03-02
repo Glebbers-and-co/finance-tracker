22351c64327f06d62a7a636a136ba74ce8f20968
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-03-02 13:42:57 +0300

    🖼️ add bg image for layout

--- src/components/layout/CoreLayout/CoreLayout.tsx
+++ src/components/layout/CoreLayout/CoreLayout.tsx
@@ -4,9 +4,11 @@ import cn from 'classnames';
 
 import Footer from '@/src/components/layout/Footer/Footer';
 import Header from '@/src/components/layout/Header/Header';
+import bgImage from '@/public/backgrounds/1614320957_69-p-foto-raduzhnogo-fona-89.jpg';
 
 import styles from './CoreLayout.module.scss';
 import type { CoreLayoutProps } from './CoreLayout.props';
+import BackgroundImage from '@/src/components/ui/BackgroundImage/BackgroundImage';
 
 /**
  * Wrapper for core layout. Provides access to fully manageable
@@ -29,6 +31,16 @@ const CoreLayout: FC<PropsWith<'children', CoreLayoutProps>> = ({
 }) => {
   return (
     <div className={cn(styles.layoutWrapper)}>
+      <BackgroundImage
+        className={cn('absolute top-0 left-0 w-full h-full')}
+        src={bgImage}
+        alt={'Background'}
+        style={{
+          objectPosition: 'center bottom',
+        }}
+        aria-hidden
+      />
+
       <Header />
 
       {children}
