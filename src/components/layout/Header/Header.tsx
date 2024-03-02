908d7ac39375faac9999c7874b6f3bacfdf720b7
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-03-02 22:26:15 +0300

    🪞 reformat code with prettier

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,12 +1,12 @@
-import { FC } from 'react';
 import cn from 'classnames';
+import { FC } from 'react';
 
-import UiContainer from '@/src/components/ui/UiContainer/UiContainer';
 import Navbar from '@/src/components/layout/Navbar/Navbar';
+import Logotype from '@/src/components/ui/Logotype/Logotype';
+import UiContainer from '@/src/components/ui/UiContainer/UiContainer';
 
 import styles from './Header.module.scss';
 import type { HeaderProps } from './Header.props';
-import Logotype from '@/src/components/ui/Logotype/Logotype';
 
 const Header: FC<HeaderProps> = () => {
   return (
