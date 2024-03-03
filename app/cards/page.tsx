089cd9fd966aab078c51b91dd91276ef427487c4
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-03-03 15:08:39 +0300

    📝 add page title

--- app/cards/page.tsx
+++ app/cards/page.tsx
@@ -1,8 +1,16 @@
+import { Metadata } from 'next';
 import { FC } from 'react';
 
 import CardListSection from '@/app/cards/sections/CardListSection/CardListSection';
 import PageBody from '@/src/components/layout/PageBody/PageBody';
 import PageHeader from '@/src/components/layout/PageHeader/PageHeader';
+import { generateStaticMetadata } from '@/src/utils/seo';
+
+export async function generateMetadata(): Promise<Metadata> {
+  return generateStaticMetadata({
+    title: 'Карты',
+  });
+}
 
 const CardsPage: FC = () => {
   return (
